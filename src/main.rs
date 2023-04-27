b4b61dc50f6e2c5137083039b1af55acdeac10c2
Joel B <joel3000@gmail.com>
2023-04-26 22:55:24 -0700

    Converting to structs/impls. Added tests.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,53 @@
+#![allow(warnings)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
 use ::phf::{phf_map};
 
 static SUITS: phf::Map<u8, &'static str> = phf_map! {
-    0u8 => "Clubs",
-    1u8 => "Diamonds",
-    2u8 => "Hearts",
-    3u8 => "Spades"
+    0u8 => "♣",
+    1u8 => "♦",
+    2u8 => "♥",
+    3u8 => "♠"
 };
 
 static PIPS: phf::Map<u8, &'static str> = phf_map! {
-    0u8 => "2 ",
-    1u8 => "3 ",
-    2u8 => "4 ",
-    3u8 => "5 ",
-    4u8 => "6 ",
-    5u8 => "7 ",
-    6u8 => "8 ",
-    7u8 => "9 ",
+    0u8 => "2",
+    1u8 => "3",
+    2u8 => "4",
+    3u8 => "5",
+    4u8 => "6",
+    5u8 => "7",
+    6u8 => "8",
+    7u8 => "9",
     8u8 => "10",
-    9u8 => "J ",
-    10u8 => "Q ",
-    11u8 => "K ",
-    12u8 => "A "
+    9u8 => "J",
+    10u8 => "Q",
+    11u8 => "K",
+    12u8 => "A"
 };
 
 #[derive(Debug)]
 struct Card {
     suit: u8,
     pip: u8,
+}
+
+impl Card {
+    fn new(number: u8) -> Result<Self, String> {
+        if number >= 1 && number <= 52 {
+            let suit = (number - 1) / 13;
+            let pip = (number - 1).rem_euclid(13);
+            return Ok(Card { suit, pip });
+        } else {
+            return Err("`number` value must be between from 1-52".to_string());
+        }
+    }
+
+    fn get_card_name(&self) -> String {
+        // Return a string representation of the card
+        // e.g. "Ace of Spades"
+        format!("{}{}", PIPS[&self.pip], SUITS[&self.suit])
+    }
 }
 
 #[derive(Debug)]
@@ -46,7 +67,7 @@ impl Deck {
             //println!("Adding card: {} to deck: {:064b}", *card, *deck);
             self.deck |= mask;
             //println!("Added card : {} current: {:064b}", *card, *deck);
-            let out = translate_card_number(&card);
+            let out = num_to_card(&card);
             println!("out card = {:?}", out);
             return Ok(out);
         } else {
@@ -93,13 +114,12 @@ fn get_cards(deck: &mut Deck) -> Vec<i32> {
     return out;
 }
 
-fn translate_card_number(card: &u8) -> Card {
-    // turn a card number(1-52) into a pip/suit name
-    let c = Card {
+fn num_to_card(card: &u8) -> Card {
+    // turn a card number(1-52) into a Card struct
+    Card {
         suit: (*card - 1) / 13,
         pip: (*card - 1).rem_euclid(13)
-    };
-    c
+    }
 }
 
 fn main() {
@@ -109,12 +129,7 @@ fn main() {
     };
     let mut card: u64 = 1;
     let cards;
-    add_card(&mut card, &mut deck).expect("foo");
-    add_card(&mut 4, &mut deck).expect("bar");
-    add_card(&mut 52, &mut deck).expect("baz");
 
-    translate_card_number(&mut 52);
-    translate_card_number(&mut 1);
 
     cards = get_cards(&mut deck);
 
@@ -128,8 +143,58 @@ mod tests {
 
     #[test]
     fn test_add_card() {
-        let mut deck: u64 = 0;
+        let mut deck: Deck = Deck { deck: 0, draws: Vec::new()};
         let out = add_card(&mut 1, &mut deck).expect("Could not insert card.");
         assert_eq!(out, true);
     }
+
+    #[test]
+    fn test_translate_card_number() {
+        let mut c: Card = Card { suit: 0, pip: 0 };
+        c = num_to_card(&mut 52);
+        assert_eq!(c.suit, 3);
+        assert_eq!(c.pip , 12);
+        c = num_to_card(&mut 1);
+        assert_eq!(c.suit, 0);
+        assert_eq!(c.pip, 0);
+        c = num_to_card(&mut 15);
+        assert_eq!(c.suit, 1); // diamonds
+        assert_eq!(c.pip, 1); // 3 of diamonds
+        c = num_to_card(&mut 36);
+        assert_eq!(c.suit, 2); // hearts
+        assert_eq!(c.pip, 9); // J of hearts
+   }
+
+   #[test]
+   fn test_get_card_name() {
+       let mut c: Card = Card { suit: 0, pip: 0 };
+       c = num_to_card(&mut 52);
+       assert_eq!(c.get_card_name(), "A♠");
+       c = num_to_card(&mut 1);
+       assert_eq!(c.get_card_name(), "2♣");
+       c = num_to_card(&mut 15);
+       assert_eq!(c.get_card_name(), "3♦");
+       c = num_to_card(&mut 36);
+       assert_eq!(c.get_card_name(), "J♥");
+       c = num_to_card(&mut 40);
+       assert_eq!(c.get_card_name(), "2♠");
+   }
+
+   #[test]
+   fn test_new_card() {
+         let mut c: Card = Card { suit: 0, pip: 0 };
+         c = Card::new(52).expect("Could not create card.");
+         assert_eq!(c.suit, 3);
+         assert_eq!(c.pip, 12);
+         c = Card::new(1).expect("Could not create card.");
+         assert_eq!(c.suit, 0);
+         assert_eq!(c.pip, 0);
+         c = Card::new(15).expect("Could not create card.");
+         assert_eq!(c.suit, 1); // diamonds
+         assert_eq!(c.pip, 1); // 3 of diamonds
+         c = Card::new(36).expect("Could not create card.");
+         assert_eq!(c.suit, 2); // hearts
+         assert_eq!(c.pip, 9); // J of hearts
+   }
+
 }
