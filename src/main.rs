fec4fce9f173fc1e79d5d1455a31743c6e7d71c6
Joel Bremson <joel3000@gmail.com>
2023-04-26 18:04:18 -0700

    Adding struct based architecture.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,88 @@
 use ::phf::{phf_map};
 
-static SUITS: phf::Map<i32, &'static str> = phf_map! {
-    0i32 => "Clubs",
-    1i32 => "Diamonds",
-    2i32 => "Hearts",
-    3i32 => "Spades"
+static SUITS: phf::Map<u8, &'static str> = phf_map! {
+    0u8 => "Clubs",
+    1u8 => "Diamonds",
+    2u8 => "Hearts",
+    3u8 => "Spades"
 };
 
-static PIPS: phf::Map<i32, &'static str> = phf_map! {
-    0i32 => "2 ",
-    1i32 => "3 ",
-    2i32 => "4 ",
-    3i32 => "5 ",
-    4i32 => "6 ",
-    5i32 => "7 ",
-    6i32 => "8 ",
-    7i32 => "9 ",
-    8i32 => "10",
-    9i32 => "J ",
-    10i32 => "Q ",
-    11i32 => "K ",
-    12i32 => "A "
+static PIPS: phf::Map<u8, &'static str> = phf_map! {
+    0u8 => "2 ",
+    1u8 => "3 ",
+    2u8 => "4 ",
+    3u8 => "5 ",
+    4u8 => "6 ",
+    5u8 => "7 ",
+    6u8 => "8 ",
+    7u8 => "9 ",
+    8u8 => "10",
+    9u8 => "J ",
+    10u8 => "Q ",
+    11u8 => "K ",
+    12u8 => "A "
 };
 
-fn add_card(card: &mut u64, deck: &mut u64) -> Result<bool, String> {
+#[derive(Debug)]
+struct Card {
+    suit: u8,
+    pip: u8,
+}
+
+#[derive(Debug)]
+struct Deck {
+    deck: u64,
+    // bit representation of the deck
+    draws: Vec<u8>, // card position numbers that have been drawn (1-52) in order
+}
+
+impl Deck {
+    fn draw_card(&mut self, card: u8) -> Result<Card, String> {
+        // Draw card number 1-52. Takes it out of the deck so that it can't be drawn again.
+        // Returns a Card struct with the card value.
+        // Side Effect adds it to the Deck draws list
+        if card >= 1 && card <= 52 {
+            let mask = 1 << card - 1;
+            //println!("Adding card: {} to deck: {:064b}", *card, *deck);
+            self.deck |= mask;
+            //println!("Added card : {} current: {:064b}", *card, *deck);
+            let out = translate_card_number(&card);
+            println!("out card = {:?}", out);
+            return Ok(out);
+        } else {
+            return Err("`card` value must be between from 1-52".to_string());
+        }
+    }
+}
+
+struct Hand {
+    cards: u64, // bit representation of a hand
+}
+// make a Deck struct that holds the u64 deck bit string
+// number of drawn cards
+
+fn add_card(card: &mut u64, deck: &mut Deck) -> Result<bool, String> {
     if *card >= 1 && *card <= 52 {
         let mask = 1 << *card - 1;
-        println!("Adding card: {} to deck: {:064b}", *card, *deck);
-        *deck |= mask;
-        println!("Added card : {} current: {:064b}", *card, *deck);
+        //println!("Adding card: {} to deck: {:064b}", *card, *deck);
+        deck.deck |= mask;
+        //println!("Added card : {} current: {:064b}", *card, *deck);
         return Ok(true);
     } else {
         return Err("`card` value must be between from 1-52".to_string());
     }
 }
 
-fn get_cards(deck: &u64) -> Vec<i32> {
+fn get_cards(deck: &mut Deck) -> Vec<i32> {
+    // Return a list of all the cards position numbers that have been drawn
+    // from the deck
     let mut bit = 52;
     let mut out = Vec::new();
     let mut mask;
     while bit > 0 {
         mask = 1 << bit - 1;
 
-        if *deck & mask != 0 {
+        if deck.deck & mask != 0 {
             out.push(bit)
             //println!("Found card at position: {}", bit);
             //println!("deck: {:064b}", *deck);
@@ -53,15 +93,20 @@ fn get_cards(deck: &u64) -> Vec<i32> {
     return out;
 }
 
-fn translate_card_number(card: &i32) {
+fn translate_card_number(card: &u8) -> Card {
     // turn a card number(1-52) into a pip/suit name
-    let suit = (*card - 1) / 13;
-    let pip = (*card - 1).rem_euclid(13);
-    println!("card: {} suit: {:?} pip: {}", *card, SUITS[&suit], PIPS[&pip]);
+    let c = Card {
+        suit: (*card - 1) / 13,
+        pip: (*card - 1).rem_euclid(13)
+    };
+    c
 }
 
 fn main() {
-    let mut deck: u64 = 0;
+    let mut deck = Deck {
+        deck: 0,
+        draws: Vec::new(),
+    };
     let mut card: u64 = 1;
     let cards;
     add_card(&mut card, &mut deck).expect("foo");
@@ -71,7 +116,7 @@ fn main() {
     translate_card_number(&mut 52);
     translate_card_number(&mut 1);
 
-    cards = get_cards(&deck);
+    cards = get_cards(&mut deck);
 
     println!("cards: {:?}", cards);
 }
@@ -87,5 +132,4 @@ mod tests {
         let out = add_card(&mut 1, &mut deck).expect("Could not insert card.");
         assert_eq!(out, true);
     }
-
 }
